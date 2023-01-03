434c68e2f0e8376e39cd1f44ee40550f477f26b3
Vladislav Mikhalchuk <vlad.mikhalchuk.01@mail.ru>
2023-01-03 14:11:50 +0300

    View patient's profile by admin

--- Clinic.Backend/Profiles/Profiles.Core/Interfaces/Data/Repositories/IPatientRepository.cs
+++ Clinic.Backend/Profiles/Profiles.Core/Interfaces/Data/Repositories/IPatientRepository.cs
@@ -9,6 +9,7 @@ public interface IPatientRepository
     public Task CreatePatientProfileAsync(Patient patient);
     public Task<Patient?> GetPatientByIdAsync(string id);
     public Task<PatientProfileByDoctorResponse> MappingToPatientProfileByDoctorResponse(Patient patient);
+    public Task<PatientProfileByAdminResponse> MappingToPatientProfileByAdminResponse(Patient patient);
     public Task<PagedList<Patient>> GetPatientsByAdminAsync(SearchParams searchParams);
     public Task<ICollection<PatientsProfileSearchByAdminResponse>> MappingToPatientsProfileSearchByAdminResponse(PagedList<Patient> patients);
     public Task<bool> IsProfileExistAsync(string firstName, string lastName, string? middleName, DateTime dateOfBirth);
---
+++ Clinic.Backend/Profiles/Profiles.Core/Logic/Profile/Responses/PatientProfileByAdminResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Profiles.Core.Logic.Profile.Responses;
+
+public record PatientProfileByAdminResponse(string firstName, string lastName, string? middleName, string phoneNumber, DateTime dateOfBirth);
