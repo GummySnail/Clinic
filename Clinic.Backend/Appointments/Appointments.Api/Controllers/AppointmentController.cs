ddae8cd135729125825dc414aa310e7aef89b2d7
Vladislav Mikhalchuk <vlad.mikhalchuk.01@mail.ru>
2023-01-11 14:16:18 +0300

    add appointment

---
+++ Clinic.Backend/Appointments/Appointments.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,33 @@
+﻿using Appointments.Api.Models.Appointment.Requests;
+using Appointments.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointments.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController : ControllerBase
+{
+    private readonly IAppointmentService _appointmentService;
+
+    public AppointmentController(IAppointmentService appointmentService)
+    {
+        _appointmentService = appointmentService;
+    }
+    
+    //[Authorize(Roles = "Patient")]
+    [HttpPost("create-appointment")]
+    public async Task<ActionResult> AddAppointment([FromBody] CreateAppointmentRequest request)
+    {
+        try
+        {
+            await _appointmentService.AddAppointmentAsync(request.AppointmentDate, request.IsApproved);
+
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
+}
---
+++ Clinic.Backend/Appointments/Appointments.Api/Models/Appointment/Validators/CreateAppointmentValidator.cs
@@ -0,0 +1,17 @@
+﻿using Appointments.Api.Models.Appointment.Requests;
+using FluentValidation;
+
+namespace Appointments.Api.Models.Appointment.Validators;
+
+public class CreateAppointmentValidator : AbstractValidator<CreateAppointmentRequest>
+{
+    public CreateAppointmentValidator()
+    {
+        RuleFor(x => x.AppointmentDate)
+            .NotNull().WithMessage("Appointment date can't be null")
+            .NotEmpty().WithMessage("Appointment date can't be empty")
+            .Must(ValidateAppointmentDate).WithMessage("Appointment date must be higher than the current one");
+    }
+
+    private bool ValidateAppointmentDate(DateTime appointmentDate) => appointmentDate > DateTime.UtcNow;
+}
