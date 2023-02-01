f5f9a4a3e97227931984ad2d6085d8f673ff9a9e
Vladislav Mikhalchuk <vlad.mikhalchuk.01@mail.ru>
2023-02-01 20:23:46 +0300

    refactoring offices microservice

--- Clinic.Backend/Offices/Offices.Infrastructure/Services/AzureService.cs
+++ Clinic.Backend/Offices/Offices.Infrastructure/Services/AzureService.cs
@@ -1,6 +1,9 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Offices.Core.Exceptions;
 using Offices.Core.Interfaces.Services;
 
 namespace Offices.Infrastructure.Services;
@@ -19,14 +22,23 @@ public class AzureService : IAzureService
     public async Task<string> UploadOfficePhotoAsync(IFormFile file)
     {
         BlobContainerClient container = new BlobContainerClient(_storageConnectionString, _storageContainerName);
-
-        BlobClient client = container.GetBlobClient(file.FileName);
-
-        await using (Stream? data = file.OpenReadStream())
+         
+        try
         {
-            await client.UploadAsync(data);
-        }
+            BlobClient client = container.GetBlobClient(file.FileName);
 
-        return client.Uri.AbsoluteUri;
+            await using (Stream? data = file.OpenReadStream())
+            {
+                await client.UploadAsync(data);
+            }
+
+            var officePhotoUrl = client.Uri.ToString();
+            
+            return officePhotoUrl;
+        }
+        catch (RequestFailedException ex) when (ex.ErrorCode == BlobErrorCode.BlobAlreadyExists)
+        {
+            throw new FileAlreadyExistException("File with that name already exists");
+        }
     }
 }
