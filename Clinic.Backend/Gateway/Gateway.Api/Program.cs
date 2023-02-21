a6d9df5047b7343b2f7093c17e0270ff448b4b88
Vlad <vlad.mikhalchuk.01@mail.ru>
2023-02-21 19:47:02 +0300

    adding ocelot file for docker

--- Clinic.Backend/Gateway/Gateway.Api/Program.cs
+++ Clinic.Backend/Gateway/Gateway.Api/Program.cs
@@ -2,9 +2,22 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+if (env=="Development")
+{
+    builder.Configuration
+        .AddJsonFile("ocelot.Development.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();    
+}
+else
+{
+    builder.Configuration
+        .AddJsonFile("ocelot.Docker.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
+
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddCors();
 
